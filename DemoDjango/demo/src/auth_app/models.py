635a246d7e699e94350791d80cf51825f1408c0d
Robee <rghosh@bncnetwork.net>
2019-10-20 12:01:43 +0400

    first commit

---
+++ DemoDjango/demo/src/auth_app/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class User(models.Models):
+	id = models.IntegerField(primary_key=True)
+
+# Create your models here.
